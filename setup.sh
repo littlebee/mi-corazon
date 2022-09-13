c81b6f55f512661a177c79c380c03e3210e80be9
Bee <bee.wilkerson@ymail.com>
2022-09-12 19:22:00 -0700

    initial setup and hw test scripts

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# echo on
+set -x
+
+# stop on all errors
+set -e
+
+TARGET_DIR="/home/pi/mi_corazon"
+
+# sudo apt-get update
+# sudo apt-get -y upgrade
+
+# audio and video needs
+sudo apt install -y ffmpeg python3-pyaudio
+sudo pip3 install pyaudio
+
+# opencv
+sudo apt install -y libatlas-base-dev libjasper-dev libqtgui4  libqt4-test libhdf5-dev
+sudo pip3 install flask opencv-contrib-python==4.5.5.62 imutils opencv-python==3.4.2.17 numpy==1.14.5
+
+# web browser
+sudo apt install -y chromium-browser
+
+# webserver (flask) and websockets
+sudo pip3 install websockets flask flask_cors
+sudo pip3 install pybase64 psutil
+
+
+# needed for the LEDs in the heart
+sudo pip3 install rpi-ws281x
+
+# Make the webapp automatically start in raspian desktop, full screen
+mkdir -p /home/pi/.config/lxsession/LXDE-pi/
+# cp $TARGET_DIR/setup/files/lxe-autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+cp $TARGET_DIR/setup/files/lxe-autostart /etc/xdg/lxsession/LXDE-pi/autostart
+# chmod +x /home/pi/.config/lxsession/LXDE-pi/autostart
+
---
+++ upload.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# this script is meant to be run from your local development machine,
+# in the scatbot project root dir
+#
+# parameters:
+# <user@host> - required.
+# <user> - optional. If user home directory is not /home/pi, provide the username only.
+#
+# example:
+# ./upload.sh pi@raspberrypi.local
+
+if [ "$1" == "" ]; then
+  echo "Error: missing parameter.  usage: sbin/upload.sh [USER@]IP_ADDRESS_OR_NAME"
+  echo "   ex:  sbin/upload.sh pi@scatbot.local"
+  exit 1
+fi
+
+targetUser=pi
+if [ "$2" != "" ]; then
+  targetUser=$2
+  echo "got user"
+  echo $targetUser
+fi
+
+# echo on
+set -x
+
+# stop on errors
+set -e
+
+TARGET_DIR="/home/$targetUser/mi_corazon"
+TARGET_HOST=$1
+
+rsync --progress --partial -avz \
+--exclude=node_modules \
+--exclude=data/ \
+--exclude=.git \
+--exclude=yolov5 \
+--exclude=results* \
+. $TARGET_HOST:$TARGET_DIR
+
